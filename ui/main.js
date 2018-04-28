663cc1a3c64930b512cec0e48463fc37ab9c31d4
Naveen Kumar Pulikonda <naveenpulikondaaiee@gmail.com>
2018-04-28 22:21:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ console.log('Loaded!');
 var img = document.getElementById('madi');
 img.onclick() = function() {
     
-    img.style.margin-left = '100px;;
-}
+    img.style.margin-left = '100px;
+};
